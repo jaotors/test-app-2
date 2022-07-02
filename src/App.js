a39ddff1c7b4d8f620f17dc05467a5864a1325d8
Joshua Turingan <turingan.joshua@gmail.com>
2022-07-02 10:47:44 +0800

    adds color game

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
-import logo from './logo.svg'
+import React, { useState } from 'react'
 import './App.css'
 
+const COLORS = ['pink', 'blue', 'green', 'yellow', 'red', 'black']
+
 function App() {
+  const [colorBoxOne, setColorBoxOne] = useState('')
+  const [colorBoxTwo, setColorBoxTwo] = useState('')
+  const [colorBoxThree, setColorBoxThree] = useState('')
+
+  const randomIndex = () => Math.floor(Math.random() * (5 + 1))
+
+  const handleRoll = () => {
+    setColorBoxOne(COLORS[randomIndex()])
+    setColorBoxTwo(COLORS[randomIndex()])
+    setColorBoxThree(COLORS[randomIndex()])
+  }
+
+  const outputColor = () => {
+    const colors = [colorBoxOne, colorBoxTwo, colorBoxThree]
+    let tempColors = []
+    for (const color of colors) {
+      tempColors = tempColors.concat({
+        num: colors.filter((c) => c === color).length,
+        color
+      })
+    }
+
+    const sortedColors = tempColors.sort((a, b) => b.num - a.num)
+    const first = sortedColors[0]
+    if (first.num > 1) {
+      return [`${first.color} x ${first.num}`, first.color]
+    } else {
+      return ['', 'draw']
+    }
+  }
+
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Hello world</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleRoll}>ROLL</button>
+      <div className='box-list'>
+        <div style={{ backgroundColor: colorBoxOne }} className='box'></div>
+        <div style={{ backgroundColor: colorBoxTwo }} className='box'></div>
+        <div style={{ backgroundColor: colorBoxThree }} className='box'></div>
+      </div>
+      <h1 style={{ color: outputColor()[1] }} className='output'>
+        {outputColor()[0]}
+      </h1>
     </div>
   )
 }
